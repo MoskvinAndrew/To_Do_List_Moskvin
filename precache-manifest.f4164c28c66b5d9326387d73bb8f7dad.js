d5490cf9eef314a9053dbafda0fa5b2e7121646b
Андрей <anastasiiapegar@gmail.com>
2021-07-26 16:44:12 +0300

    Updates

---
+++ precache-manifest.f4164c28c66b5d9326387d73bb8f7dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4074e30fc9c82364ef73e8c461d99379",
+    "url": "/To_Do_List_Moskvin/index.html"
+  },
+  {
+    "revision": "7df8064d6365fe66c002",
+    "url": "/To_Do_List_Moskvin/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "80511fb59c7f397026b4",
+    "url": "/To_Do_List_Moskvin/static/js/2.74e52aee.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/To_Do_List_Moskvin/static/js/2.74e52aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df8064d6365fe66c002",
+    "url": "/To_Do_List_Moskvin/static/js/main.220bea70.chunk.js"
+  },
+  {
+    "revision": "1d11edb01fc3ace1e3f6",
+    "url": "/To_Do_List_Moskvin/static/js/runtime-main.12b8174d.js"
+  }
+]);
